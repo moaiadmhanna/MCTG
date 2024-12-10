d0ac62f4549356ac6d008fb40fd72f6f5137bd4a
muayad <moaiadmhana999@gmail.comm>
2024-12-11 00:39:02 +0100

    added the Unittest solution

---
+++ MCTG_UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MCTG_UnitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
